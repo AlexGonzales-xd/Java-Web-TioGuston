d7db7c13bf51f667dea11c9c4ba72b957a6a5391
AlexGonzales-xd <alexjonasgs107@gmail.com>
2026-04-17 16:18:16 -0500

    Add files via upload

---
+++ proyectosenati/FrontEnd/js/main.js
@@ -0,0 +1,97 @@
+// EVENTOS EN JAVASCRIPT (CLICK, CARGAR, KEY,)
+document.addEventListener("DOMContentLoaded"), () => {
+    fetch("http://localhost:8080/api/productos")
+    .then((response) => response.json())
+    .then((data) => {
+         // DOM -> <tbody id="table-producto">
+         const elemento = document.getElementById("table-producto");
+         for (let i = 0; i < data.length; i++) {
+         //data[i], muestra en forma de array
+         let cliente = data[i];
+         // alt + 96
+         let fila = `
+                            <tr>
+                            <td>${producto.id}</td>
+                            <td>${producto.nombre}</td>
+                            <td>${producto.descripcion}</td>
+                            <td>${producto.precio}</td>
+                            <td>
+                                <button
+                                    class="btn btn-outline-primary me-2"
+                                     id="btnEditar"
+                                     data-bs-toggle="modal" 
+                                     data-bs-target="#modalEditarProducto"
+                                     data-idprod=${producto.id}
+                                     data-nomprod=${producto.nombre}
+                                     data-descriprod=${producto.descripcion}
+                                     data-precioprod=${producto.precio}
+                                    >
+                                     <i class="fas fa-edit"></i> Editar
+                                </button>
+                                <button id="btnEliminar" data-idproducto = ${producto.id} class="btn btn-outline-danger">
+                                    <i class="fas fa-trash"></i> Eliminar
+                                </button>
+                            </td>
+                            </tr>                
+                           `;
+        elemento.innerHTML += fila;
+        }
+    });
+
+    // DOM (document object model) Dar accion al boton de guardar producto
+    const btnSaveProducto = document.getElementById("btn-crearproducto");
+    btnSaveCliente.addEventListener("click", guardarProducto);
+};
+// EVENTO DE CLICK EN JAVASCRIPT
+//Creamos una variable que almacene el DOM de ese elemento del boton
+document.addEventListener("click", function (e) {
+  const btnDelete = e.target.closest("#btnEliminar");
+  if (btnDelete) {
+    //TRUE o 1
+    alert("Eliminando...");
+    const id = btnDelete.dataset.idproducto;
+    //console.log(id) para en consola que ID es nada mas
+    //fetch("http://localhost:8080/api/productos/"+id, {
+    fetch(`http://localhost:8080/api/productos/${id}`, {
+      method: "DELETE",
+    }).then((response) => {
+      if (response.ok) {
+        alert("Producto eliminado correctamente");
+        location.reload(); // Recargar la página para reflejar los cambios
+      } else {
+        alert("Error al eliminar el Producto");
+      }
+    });
+  }
+});
+// CREAMOS UN FUNCION BASICA
+function guardarProducto() {
+  const nombre = document.getElementById("c_nombre").value;
+  const descripcion = document.getElementById("c_descripcion").value;
+  const precio = document.getElementById("c_precio").value;
+  console.log(nombre, descripcion,precio);
+  fetch("http://localhost:8080/api/productos", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ nombre,descripcion,precio}),
+  }).then((response) => {
+    console.log(response); //mensaje en la consola (200 o OK)
+    if (response.ok) {
+      location.reload();
+    } else {
+      alert("Error: no se pudo guardar");
+    }
+  });
+}
+
+//funcion para poner los datos en el input del FORMULARIO actualizar
+function llamardatos() {
+  const btnEditar = e.target.closest("#btnEditar");
+  const id_prod= btnEditar.dataset.idprod;
+  const nom_prod = btnEditar.dataset.nomprod;
+  const descri_prod = btnEditar.dataset.descriprod;
+  const precio_prod = btnEditar.dataset.precioprod;
+  document.getElementById("c_u_nombre").value = nom_prod;
+  document.getElementById("c_u_descripcion").value = descri_prod;
+  document.getElementById("c_u_precio").value = precio_prod;
+}
